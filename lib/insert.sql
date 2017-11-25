b755b70687b431d5b6bb63e7eb5cf390a93f6e75
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-11-25 13:53:55 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,24 @@
-INSERT INTO series (title, author_id, subgenre_id)
+INSERT INTO series (title, author_id, subgenre_id) values
+INSERT INTO series (title, author_id, subgenre_id) values
 
-INSERT INTO subgenres (name)
+INSERT INTO subgenres (name) values
+INSERT INTO subgenres (name) values
 
-INSERT INTO authors (name)
+INSERT INTO authors (name) values
+INSERT INTO authors (name) values
 
-INSERT INTO books (title, year, series_id)
+INSERT INTO books (title, year, series_id) values
+INSERT INTO books (title, year, series_id) values
+INSERT INTO books (title, year, series_id) values
+INSERT INTO books (title, year, series_id) values
+INSERT INTO books (title, year, series_id) values
+INSERT INTO books (title, year, series_id) values
 
-INSERT INTO characters  (name, motto, species, author_id, subgenre_id)
+INSERT INTO characters  (name, motto, species, author_id, subgenre_id) values
+INSERT INTO characters  (name, motto, species, author_id, subgenre_id) values
+INSERT INTO characters  (name, motto, species, author_id, subgenre_id) values
+INSERT INTO characters  (name, motto, species, author_id, subgenre_id) values
+INSERT INTO characters  (name, motto, species, author_id, subgenre_id) values
+INSERT INTO characters  (name, motto, species, author_id, subgenre_id) values
+INSERT INTO characters  (name, motto, species, author_id, subgenre_id) values
+INSERT INTO characters  (name, motto, species, author_id, subgenre_id) values
