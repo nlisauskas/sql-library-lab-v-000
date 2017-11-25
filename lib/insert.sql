b8bbab78440eeee481788c18a662694d5c829b38
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-11-25 13:52:14 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO series (title, author_id, subgenre_id)
+
+INSERT INTO subgenres (name)
+
+INSERT INTO authors (name)
+
+INSERT INTO books (title, year, series_id)
+
+INSERT INTO characters  (name, motto, species, author_id, subgenre_id)
