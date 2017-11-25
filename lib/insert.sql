adfae01c8bf62dfdded60a2303d76b3618c3455d
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-11-25 13:56:17 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO series (title, author_id, subgenre_id) values
-INSERT INTO series (title, author_id, subgenre_id) values
+INSERT INTO series (title, author_id, subgenre_id) values ("Harry Potter", 1, 1)
+INSERT INTO series (title, author_id, subgenre_id) values ("Lord of the Rings", 2, 2)
 
-INSERT INTO subgenres (name) values
-INSERT INTO subgenres (name) values
+INSERT INTO subgenres (name) values ("Wizardry")
+INSERT INTO subgenres (name) values ("Sci-fi")
 
 INSERT INTO authors (name) values
 INSERT INTO authors (name) values
